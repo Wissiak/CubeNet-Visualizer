5ac30f7209df8bedb57df1c9b8b1dba33c5bf087
NOEL BRAND <noel.brand@ost.ch>
2024-02-06 15:22:58 +0100

    Added Toggle Button Adjusted Burger Menu

--- digicamp/src/Navigation.js
+++ digicamp/src/Navigation.js
@@ -5,24 +5,47 @@ import "./Navigation.css";
 
 function Navigation() {
   const [collapsed, setCollapsed] = useState(true);
-  const sidebarWidth = 200;
+  function Toggle() {
+    const [isToggled, setIsToggled] = useState(false);
+
+    const toggle = () => {
+      setIsToggled(!isToggled);
+    };
+
+    return (
+      <div className={`toggle-container${isToggled ? ' toggled' : ''}`} onClick={toggle}>
+        <div className={`toggle-button ${isToggled ? 'toggled' : ''}`}></div>
+      </div>
+    );
+  }
+  const sidebarWidth = 250;
   return (
     <span>
       <img
         src={hamburger}
         width={40}
-        style={{ left: collapsed ? 10 : sidebarWidth + 10}}
+        style={{ left: collapsed ? 10 : sidebarWidth + 10 }}
         className="hamburger"
         onClick={() => setCollapsed(!collapsed)}
       />
       <Sidebar collapsed={collapsed} collapsedWidth={0} style={{ height: "100vh" }} width={`${sidebarWidth}px`}>
         <Menu>
-          <SubMenu label="Charts">
-            <MenuItem> Pie charts </MenuItem>
-            <MenuItem> Line charts </MenuItem>
+          <div className="toggle-sides-container">
+            <label className="toggle-sides-label">Enable Highlighting</label><Toggle></Toggle>
+          </div>
+          <SubMenu label="Cube Nets">
+            <MenuItem> Net1 </MenuItem>
+            <MenuItem> Net2 </MenuItem>
+            <MenuItem> Net3 </MenuItem>
+            <MenuItem> Net4 </MenuItem>
+            <MenuItem> Net5 </MenuItem>
+            <MenuItem> Net6 </MenuItem>
+            <MenuItem> Net7 </MenuItem>
+            <MenuItem> Net8 </MenuItem>
+            <MenuItem> Net9 </MenuItem>
+            <MenuItem> Net10 </MenuItem>
+            <MenuItem> Net11 </MenuItem>
           </SubMenu>
-          <MenuItem> Documentation </MenuItem>
-          <MenuItem> Calendar </MenuItem>
         </Menu>
       </Sidebar>
     </span>
