d0a9efa7e8e148b1e9717c0eeca3f9a9efe35149
patrick.wissiak <patrick.wissiak@ost.ch>
2024-02-07 12:11:24 +0100

    Add chevrons and fix animation bug

--- digicamp/src/AnimationController.js
+++ digicamp/src/AnimationController.js
@@ -1,36 +1,56 @@
 import React from "react";
 import "./AnimationController.css";
 
+let lastAction = new Date().getTime() - 1000; 
 
-function AnimationControls({setStep, currentStep, onStepChange}) {
-
-  const numberOfSteps = 6
+function AnimationControls({ setStep, currentStep, onStepChange }) {
+  const numberOfSteps = 6;
 
   function nextStep() {
+    const now = new Date().getTime();
+    if (now - lastAction < 1000) {
+      // wait until animation is over to not skip any steps
+      return;
+    }
     onStepChange(currentStep, currentStep + 1);
     setStep(Math.min(currentStep + 1, numberOfSteps));
+    lastAction = now;
   }
 
   function prevStep() {
+    const now = new Date().getTime();
+    if (now - lastAction < 1000) {
+      // wait until animation is over to not skip any steps
+      return;
+    }
     onStepChange(currentStep, currentStep - 1);
-    setStep(Math.max(currentStep - 1,  1));
+    setStep(Math.max(currentStep - 1, 1));
+    lastAction = now;
   }
 
   return (
     <div className="animation-control">
-      <button
+      <img
+        className="chevron"
+        src="chevron-left.webp"
+        alt="Previous"
         id="control-prev"
         disabled={currentStep <= 1}
         onClick={prevStep}
-      >Prev</button>
-      <p id="control-status">{currentStep}&nbsp;/&nbsp;{numberOfSteps}</p>
-      <button
+      />
+      <p id="control-status">
+        {currentStep}&nbsp;/&nbsp;{numberOfSteps}
+      </p>
+      <img
+        className="chevron"
+        src="chevron-right.webp"
+        alt="Next"
         id="control-next"
         disabled={currentStep >= numberOfSteps}
         onClick={nextStep}
-      >Next</button>
+      />
     </div>
-  )
+  );
 }
 
 export { AnimationControls };
